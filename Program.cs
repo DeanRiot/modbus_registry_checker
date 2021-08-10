086ec0248fc72f4ccce80283206db6f12ed04ea9
Dmitry Tarakanovsky <didos.developer@gmail.com>
2021-08-10 10:50:54 +0300

    bug fix

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.IO.Ports;
 using System.Threading;
 
@@ -14,28 +15,43 @@ namespace RegChecker
             ushort startRegister;
             ushort endRegister;
             ushort range;
-            if (args == null)
+            bool saveToFile = false;
+            var result = new System.Collections.Generic.List<string>();
+
+            if (args.Length == 0)
             {
-                Console.WriteLine("номер порта:");
-                portNum = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine("Port:");
+                    portNum = Console.ReadLine();
 
-                Console.WriteLine("ID Slave:");
-                slave = Console.ReadLine();
+                    Console.WriteLine("ID Slave:");
+                    slave = Console.ReadLine();
 
-                Console.WriteLine("Команда:");
-                command = Console.ReadLine();
+                    Console.WriteLine("Command:");
+                    command = Console.ReadLine();
 
-                Console.WriteLine("От какого регистра");
-                startRegister = ushort.Parse(Console.ReadLine());
+                    Console.WriteLine("From register:");
+                    startRegister = ushort.Parse(Console.ReadLine());
 
-                Console.WriteLine("До какого регистра");
-                endRegister = ushort.Parse(Console.ReadLine());
+                    Console.WriteLine("To register:");
+                    endRegister = ushort.Parse(Console.ReadLine());
 
-                Console.WriteLine("Количество регистров");
-                range = ushort.Parse(Console.ReadLine());
+                    Console.WriteLine("Range:");
+                    range = ushort.Parse(Console.ReadLine());
 
+                    Console.WriteLine("Save to File?");
+                    saveToFile = Console.ReadLine().Equals("y") ? true : false;
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error in parameter");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return;
+                }
             }
-            else if (args[0].Equals("help"))
+            else if (args.Length == 1 && args[0].Equals("help"))
             {
                 Console.WriteLine("Command line: [COM1] [SlaveID] [COMMAND] [from reg] [to reg] [range of regs] (File)\n" +
                                     "Commands: \n" +
@@ -43,10 +59,10 @@ namespace RegChecker
                                     "InputRegs - enumerate discrete input regs \n" +
                                     "Inputs - enumerate input regs \n" +
                                     "Holds - enumerate hold regs(range is important) \n" +
-                                    "File - (optional) save to txt");
+                                    "File - (optional) save to txt (y/n)");
                 return;
             }
-            else
+            else if (args.Length == 5 || args.Length == 6)
             {
                 try
                 {
@@ -56,17 +72,41 @@ namespace RegChecker
                     startRegister = ushort.Parse(args[3]);
                     endRegister = ushort.Parse(args[4]);
                     range = ushort.Parse(args[5]);
+                    saveToFile = args[6] != null && args[6].Equals("y") ? true : false;
                 }
                 catch
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Error in parameters");
+                    Console.ForegroundColor = ConsoleColor.White;
                     return;
                 }
             }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: Invalid number of parameters");
+                Console.ForegroundColor = ConsoleColor.White;
+                return;
+            }
+
             var port = new SerialPort(portNum, 9600, Parity.None, 8, StopBits.One);
 
-            port.Open();
+            try
+            {
+                Console.WriteLine("Opening port");
+                port.Open();
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{portNum} busy or not connected ");
+                Console.ForegroundColor = ConsoleColor.White;
+                return;
+            }
+
             var modb = Modbus.Device.ModbusSerialMaster.CreateRtu(port);
+
             switch (command)
             {
                 case "Coils":
@@ -74,12 +114,18 @@ namespace RegChecker
                     {
                         try
                         {
-                            Console.WriteLine($"on reg: {startRegister} {modb.ReadCoils(byte.Parse(slave), startRegister, range)}");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            var message = $"on reg: {startRegister} {modb.ReadCoils(byte.Parse(slave), startRegister, range)}";
+                            Console.WriteLine(message);
+                            if (saveToFile) result.Add(message);
                             Thread.Sleep(500);
                         }
                         catch
                         {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            var message = $"on reg: {startRegister} none";
                             Console.WriteLine($"on reg: {startRegister} none");
+                            if (saveToFile) result.Add(message);
                         }
 
                     }
@@ -91,12 +137,18 @@ namespace RegChecker
                     {
                         try
                         {
-                            Console.WriteLine($"on reg: {startRegister} {modb.ReadInputRegisters(byte.Parse(slave), startRegister, range)}");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            var message = $"on reg: {startRegister} {modb.ReadInputRegisters(byte.Parse(slave), startRegister, range)}";
+                            Console.WriteLine(message);
+                            if (saveToFile) result.Add(message);
                             Thread.Sleep(500);
                         }
                         catch
                         {
-                            Console.WriteLine($"on reg: {startRegister} none");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            var message = $"on reg: {startRegister} none";
+                            Console.WriteLine(message);
+                            if (saveToFile) result.Add(message);
                         }
                     }
 
@@ -108,12 +160,18 @@ namespace RegChecker
                     {
                         try
                         {
-                            Console.WriteLine($"on reg: {startRegister} { modb.ReadInputs(byte.Parse(slave), startRegister, range)}");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            var message = $"on reg: {startRegister} { modb.ReadInputs(byte.Parse(slave), startRegister, range)}";
+                            Console.WriteLine(message);
+                            if (saveToFile) result.Add(message);
                             Thread.Sleep(500);
                         }
                         catch
                         {
-                            Console.WriteLine($"on reg: {startRegister} none");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            var message = $"on reg: {startRegister} none";
+                            Console.WriteLine(message);
+                            if (saveToFile) result.Add(message);
                         }
                     }
 
@@ -126,13 +184,19 @@ namespace RegChecker
                     {
                         try
                         {
-                            Console.WriteLine($"on reg: {startRegister} {modb.ReadHoldingRegisters(byte.Parse(slave), startRegister, range)}");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            var message = $"on reg: {startRegister} {modb.ReadHoldingRegisters(byte.Parse(slave), startRegister, range)}";
+                            Console.WriteLine(message);
+                            if (saveToFile) result.Add(message);
                             Thread.Sleep(250);
                         }
 
                         catch (Exception e)
                         {
-                            Console.WriteLine($"on reg: {startRegister} {(e.Message.Contains("131") ? "Not correct lenght or adress" : " ") }");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            var message = $"on reg: {startRegister} {(e.Message.Contains("131") ? "Not correct lenght or adress" : " ") }";
+                            Console.WriteLine(message);
+                            if (saveToFile) result.Add(message);
                             continue;
                         }
                     }
@@ -140,16 +204,34 @@ namespace RegChecker
                     break;
 
                 default:
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine($"Comands: Coils," +
                         $"InputRegs," +
                         $"Inputs," +
                         $"Holds");
                     break;
-
             }
+
             port.Close();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (saveToFile)
+            {
+
+                var fileName = DateTime.Now.ToString();
+                Console.WriteLine($"Saving Data To File {fileName.Replace(" ","_")}");
+                using (StreamWriter sw = new StreamWriter(Path.GetFullPath($"reports/{fileName}.txt")))
+                {
+                    foreach (string repotrString in result)
+                    {
+                        sw.WriteLine(repotrString);
+                    }
+
+                }
+            }
             Console.WriteLine("Job Done");
             Console.ReadKey();
         }
+
     }
 }
